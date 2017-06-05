fc2c56f306b32aad7c7cd4a06b7db20f854b5c7b
oluwasayo12 <oluwasayo12@gmail.com>
2017-06-05 11:30:24 +0100

    Create dusupaygateway.php

---
+++ modules/gateways/callback/dusupaygateway.php
@@ -0,0 +1,54 @@
+<?php
+//////////////////////////////////////////////////////
+//***************************************************/
+//* Please see the ReadMe.txt file for instruction  */
+//* This File is Written For Voguepay Gateway       */
+//* For Any Help, Contact me                        */
+//***************************************************/
+//* Email: mbosinwa@mbosinwa.me                     */
+//* Phone: 08163429760                              */
+//* Website: http://www.mbosinwa.me                 */ 
+//* WebHost: http://www.hostmeout.com.ng            */ 
+//////////////////////////////////////////////////////
+# Required File Includes
+//include("../../../dbconnect.php");
+//include("../../../includes/functions.php");
+//include("../../../includes/gatewayfunctions.php");
+//include("../../../includes/invoicefunctions.php");
+require_once __DIR__ . '/../../../init.php';
+require_once __DIR__ . '/../../../includes/functions.php'; 
+require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
+require_once __DIR__ . '/../../../includes/invoicefunctions.php';
+//$gatewaymodule = "voguepaygateway.php"; # Enter your gateway module name here replacing template
+$gatewayModuleName = basename(__FILE__, '.php');
+$GATEWAY = getGatewayVariables($gatewaymodule);
+if (!$GATEWAY["type"]) die("Module Not Activated"); # Checks gateway module is active before accepting callback
+// $transaction_id = $_POST['transaction_id'];
+// $data = file_get_contents("https://voguepay.com/?v_transaction_id=".$transaction_id."&type=xml");
+$data = file_get_contents("http://sandbox.dusupay.com/transactions/check_status/param1/param2.json");
+$transaction = json_decode($data, true);
+# Get Returned Variables - Adjust for Post Variable Names from your Gateway's Documentation
+$invoice_id = $transaction['dusupay_transactionReference'];
+$status = $transaction['status'];
+$transid = $transaction['dusupay_transactionId'];
+$invoiceid = checkCbInvoiceID($invoice_id,$GATEWAY["name"]); 
+checkCbTransID($transid); 
+if ($status =="COMPLETE" ) {
+     //Successful
+    addInvoicePayment($invoice_id,$transid,$amount,$fee,$gatewaymodule); 
+    logTransaction($GATEWAY["name"],$_POST,"Transaction Was Successful");
+}
+else
+{
+logTransaction($GATEWAY["name"],$_POST,"Transaction Not Approved OR Unrecognised Merchant ID");
+}
+if ($status =="CANCELLED") {
+    logTransaction($GATEWAY["name"],$_POST,"CANCELLED");
+}
+if ($status =="FAILED") {
+    logTransaction($GATEWAY["name"],$_POST,"FAILED");
+}
+if ($status =="PENDING") {
+    logTransaction($GATEWAY["name"],$_POST,"PENDING");
+}
+?>
